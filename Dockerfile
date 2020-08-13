2f0a65d89cc16e4c1f71dadb7d19f6dc663aa485
warren.caruana@webriq.services <warrencaruana1@gmail.com>
2020-08-13 14:51:37 +0800

    Chang texts

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-FROM nginx:1.17.1-alpine
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY public /usr/share/nginx/html
+FROM gatsbyjs/gatsby:onbuild as build
+
+FROM gatsbyjs/gatsby
+COPY public /pub
---
+++ public/component---src-pages-index-js-dee5ae7d0e79a5ca8d96.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return u}));var o=t("q1tI"),r=t.n(o);function u(){return r.a.createElement("div",null,"Hello world sss!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-dee5ae7d0e79a5ca8d96.js.map
